9f0b485e6e5c8def566ea0d14e77757053c8a37d
sitranto <anatoliyl2006@gmail.com>
2025-11-04 01:23:59 +0300

    Разметка компонента

---
+++ src/Calendar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  date: { type: Date, default: new Date(), required: false },
+})
+
+const date = ref(props.date)
+const isCalendarOpened = ref(false)
+
+</script>
+
+<template>
+  <div>
+    <input
+      type="text"
+      @click="isCalendarOpened = true"
+      readonly
+      placeholder="Выберите дату"
+    />
+    <div v-if="isCalendarOpened">
+      <div> <!-- Заголовок компонента -->
+        <button />
+        <span></span>
+        <button />
+      </div>
+
+      <div> <!-- Строка с днями недели -->
+        <div></div>
+      </div>
+
+      <div> <!-- Матрица с днями недели -->
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
