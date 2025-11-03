97f003b0b866b4b6b110063cb9c1450660e64db5
sitranto <anatoliyl2006@gmail.com>
2025-11-04 02:13:58 +0300

    Логика отображения дней

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -9,6 +9,9 @@ const monthNames = [
   'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь',
   'Ноябрь', 'Декабрь'
 ]
+const weekDayNames = [
+  'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'
+]
 
 const date = ref(props.date)
 
@@ -17,6 +20,14 @@ const currentMonth = ref(date.value.getMonth())
 const isCalendarOpened = ref(false)
 
 const currentMonthName = computed(() => monthNames[currentMonth.value])
+const daysInMonth = computed(() => {
+  return new Date(currentYear.value, currentMonth.value + 1, 0).getDate()
+})
+const firstDayOfMonthIndex = computed(() => {
+  const firstDayIndex =
+    new Date(currentYear.value, currentMonth.value, 1).getDay()
+  return (firstDayIndex + 6) % 7
+})
 
 </script>
 
@@ -28,22 +39,42 @@ const currentMonthName = computed(() => monthNames[currentMonth.value])
       readonly
       placeholder="Выберите дату"
     />
-    <div v-if="isCalendarOpened">
+    <div v-if="isCalendarOpened" class="calendar">
       <div> <!-- Заголовок компонента -->
         <button> < </button>
         <span>{{ currentMonthName }} {{ currentYear }}</span>
         <button> > </button>
       </div>
 
-      <div> <!-- Строка с днями недели -->
-        <div></div>
+      <div class="weekdays"> <!-- Строка с днями недели -->
+        <div v-for="weekDayName in weekDayNames" :key="weekDayName">
+          {{ weekDayName }}
+        </div>
       </div>
 
-      <div> <!-- Матрица с днями недели -->
-
+      <div class="days"> <!-- Матрица с днями недели -->
+        <div v-for="i in firstDayOfMonthIndex" :key="'empty-' + i"></div>
+        <div
+          v-for="day in daysInMonth"
+          :key="day"
+        >
+          {{ day }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.calendar {
+  width: 400px
+}
+.weekdays {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+.days {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+</style>
